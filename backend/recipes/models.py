d16001012d54e02292125f74fb02f0ca4db4ef79
Elena Plakhotnaya <lena.9.12@yandex.ru>
2024-11-01 15:59:30 +0300

    54

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -122,8 +122,7 @@ class Recipe(models.Model):
             MaxValueValidator(MAX)
         ]
     )
-    short_link = models.URLField(
-        max_length=SHORT_LINK_LENGTH, unique=True, blank=True, null=True)
+    short_link = models.URLField(unique=True, blank=True, null=True)
     full_link = models.URLField(blank=True, null=True)
 
     class Meta:
